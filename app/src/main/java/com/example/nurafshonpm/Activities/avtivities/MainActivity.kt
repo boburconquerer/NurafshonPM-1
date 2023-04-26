758445b0e38b8d382f509ba2e4e896e9f1035da0
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-04-26 22:23:59 +0500

    I have created some important files that you can write your code inside of them

--- app/src/main/java/com/example/nurafshonpm/Activities/avtivities/MainActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/avtivities/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.nurafshonpm.Activities
+package com.example.nurafshonpm.Activities.avtivities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import com.example.nurafshonpm.Activities.avtivities.login.SignInActivity
 import com.example.nurafshonpm.R
 
 class MainActivity : AppCompatActivity() {
