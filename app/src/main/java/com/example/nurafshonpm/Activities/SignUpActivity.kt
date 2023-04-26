51c9d6ad19621735362e7315b3884f17289786e1
User <boburabdugoffarov@gmail.com>
2023-04-26 10:10:56 -0700

    This is Project

---
+++ app/src/main/java/com/example/nurafshonpm/Activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nurafshonpm.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nurafshonpm.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
