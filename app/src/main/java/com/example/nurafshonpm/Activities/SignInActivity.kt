c77c87c791a067e9273c98932501e4678f96b0be
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-04-26 22:10:09 +0500

    welcome

--- app/src/main/java/com/example/nurafshonpm/Activities/SignInActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/SignInActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nurafshonpm.R
 
+/// hello welcome team github project
+
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
