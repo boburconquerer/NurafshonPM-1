18950f5f4788c8a826715fe1d131e29b70c9902d
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-04-26 22:56:12 +0500

    new one

--- app/src/main/java/com/example/nurafshonpm/Activities/avtivities/activity/MainActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/avtivities/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nurafshonpm.Activities.avtivities
+package com.example.nurafshonpm.Activities.avtivities.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
