058c688ab277b629d96afe8e535f2999980870f9
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2023-04-26 22:33:24 +0500

    I have created some important files

--- app/src/main/java/com/example/nurafshonpm/Activities/avtivities/login/SignInActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/avtivities/login/SignInActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nurafshonpm.R
 
-/// hello welcome team github project
+/// hello welcome team github projectsdsdsdsdfsdfdsf
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
