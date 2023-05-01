f833ed0b18b5c90b5a2b45d44e66eda0aa795e77
User <boburabdugoffarov@gmail.com>
2023-05-01 10:00:39 -0700

    added some home page

--- app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/profile/TimeManagementActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/profile/TimeManagementActivity.kt
@@ -2,6 +2,7 @@ package com.example.nurafshonpm.Activities.activities.activity.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nurafshonpm.R
 
 class TimeManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/model/ModelHomePage.kt
@@ -0,0 +1,3 @@
+package com.example.nurafshonpm.Activities.activities.model
+
+data class ModelHomePage(var imageAdministration: Int, var dateAdministration: String, var titleAdministration: String, var descriptionsAdministration: String)
