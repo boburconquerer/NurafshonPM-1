df5833b410f0caa68c856eac3f769ec1798235f8
Paxriddinov <130370961+Paxriddinov@users.noreply.github.com>
2023-05-01 22:40:21 +0500

    added daily plan and schedule

--- app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/profile/TimeManagementActivity.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/profile/TimeManagementActivity.kt
@@ -2,10 +2,36 @@ package com.example.nurafshonpm.Activities.activities.activity.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TableLayout
+import androidx.viewpager.widget.ViewPager
+import com.example.nurafshonpm.Activities.activities.activity.profile.TMfragments.dailyFragment
+import com.example.nurafshonpm.Activities.activities.activity.profile.TMfragments.tasksfragment
+import com.example.nurafshonpm.Activities.activities.adapters.TimeManageAdapter
+import com.example.nurafshonpm.R
+import com.example.nurafshonpm.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class TimeManagementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_management)
+
+
+        initView()
+
     }
+
+    private fun initView() {
+        val tabLayout = findViewById<TabLayout>(R.id.timeTbLayout_id)
+        val viewPager = findViewById<ViewPager>(R.id.timeViewPager_id)
+        val adapter = TimeManageAdapter(supportFragmentManager)
+        adapter.addFragment(dailyFragment(), "Daily plan")
+        adapter.addFragment(tasksfragment(), "Schedule")
+
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+
 }
---
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/adapters/TimeManageAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.nurafshonpm.Activities.activities.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+
+class TimeManageAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentTitleList = ArrayList<String>()
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun getItem(position: Int):Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return fragmentTitleList[position]
+    }
+    fun addFragment(fragment: Fragment, title:String){
+        fragmentList.add(fragment)
+        fragmentTitleList.add(title)
+    }
+}
