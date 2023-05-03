4a5d2c3b424b6ffab3935ae99f7dbfb54cc5d726
Youtube <muhammadziyonps@gmail.com>
2023-05-03 18:29:37 +0500

    I change for you a Profile FragmentS

---
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/EducatorsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nurafshonpm.Activities.activities.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nurafshonpm.R
+
+class EducatorsActivity : AppCompatActivity() {
+    //MNMBNMJHJH
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_educators)
+    }
+}
