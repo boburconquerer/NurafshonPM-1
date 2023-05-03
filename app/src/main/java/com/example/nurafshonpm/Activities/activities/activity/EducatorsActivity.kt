6a75141c6ec75e01aabd2a01720501620a817ad9
Youtube <muhammadziyonps@gmail.com>
2023-05-03 18:29:37 +0500

    I change for you a Profile Fragment

---
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/EducatorsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nurafshonpm.Activities.activities.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nurafshonpm.R
+
+class EducatorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_educators)
+    }
+}
--- app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/HomePage/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/nurafshonpm/Activities/activities/activity/HomePage/fragments/ProfileFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import com.example.nurafshonpm.Activities.activities.activity.EducatorsActivity
 import com.example.nurafshonpm.Activities.activities.activity.profile.TimeManagementActivity
 import com.example.nurafshonpm.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ProfileFragment : Fragment() {
 //hehe
@@ -17,20 +19,60 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
         initViews(view)
-        return  view;
+        return view;
 
     }
 
     private fun initViews(view: View) {
+        openPage(view)
+
+    }
+
+    private fun openPage(view: View) {
         val timeManage = view.findViewById<LinearLayout>(R.id.timeManagement)
         timeManage.setOnClickListener {
-            val intent = Intent(requireContext() , TimeManagementActivity::class.java)
+            val intent = Intent(requireContext(), TimeManagementActivity::class.java)
+            startActivity(intent)
+        }
+        val educatorOpen = view.findViewById<LinearLayout>(R.id.educators)
+        educatorOpen.setOnClickListener {
+            val intent = Intent(requireContext(), EducatorsActivity::class.java)
             startActivity(intent)
         }
 
+        val textNewPlan = view.findViewById<LinearLayout>(R.id.director)
+        textNewPlan.setOnClickListener {
+            bottomSheetDialog(view)
+        }
+
+
+        val textNewPlan2 = view.findViewById<LinearLayout>(R.id.adminstration)
+        textNewPlan2.setOnClickListener {
+            adminstration(view)
+        }
+
+
+    }
+
+    private fun bottomSheetDialog(view: View) {
+
+        val dialog = BottomSheetDialog(requireContext())
+        dialog.setContentView(R.layout.bottomsheet_fragment)
+        dialog.show()
+    }
+
+    private fun adminstration(view: View) {
+
+        val dialog2 = BottomSheetDialog(requireContext())
+        dialog2.setContentView(R.layout.bottomsheet_fragment)
+        dialog2.show()
     }
 }
+
+
+
+
+
